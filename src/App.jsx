8e05385480c7feff3516a03dbd1abfdd21998956
Leksa <lexysnake@gmail.com>
2023-04-12 21:21:30 +0300

    Модификация Promo с параметрами

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 // let b = 4;
 
-const Promo = () => {
+const Promo = (props) => {
+    // props - объект, в который можно передать разные свойства для компонента
+    let name = "promo";
+    switch (props.type) {
+        case "lg": name = "promo big"; break;
+        case "sm": name = "promo small"; break;
+        default: name = "promo";
+    }
+
+    
     return (
-        <div className="promo">
-            <div className="promo-pic"></div>
-            <h3>Promo text</h3>
+        <div className={name}>
+            <div className={props.pic ? "promo-pic" : "promo-pic pic2"}></div>
+            <h3>{props.text}</h3>
         </div>
     )
 }
@@ -21,11 +30,13 @@ const App = () => {
         <h2>
             <mark>React</mark>
         </h2>
-        <Promo/>
-        <Promo/>
-        <Promo/>
-        <Promo/>
-        <Promo/>
+        <div className="container">
+            <Promo text="My Promo" type="lg"/>
+            <Promo text="^_^" />
+            <Promo text="Doggy" pic={false}/>
+            <Promo type="sm"/>
+            <Promo text="Leksa is worst teacher in the Moscow" type="sm"/>
+        </div>
     </div>
 }
 
