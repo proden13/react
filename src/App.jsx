02a7cfbdc4032b6aae9aaf62d74d7a28ffbcdd9c
Leksa <admin@petiteweb.ru>
2023-04-15 14:09:15 +0300

    Переместили Promo в отдельную папку

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-// let b = 4;
-
-const Promo = (props) => {
-    // props - объект, в который можно передать разные свойства для компонента
-    let name = "promo";
-    switch (props.type) {
-        case "lg": name = "promo big"; break;
-        case "sm": name = "promo small"; break;
-        default: name = "promo";
-    }
-
-    return (
-        <div className={name}>
-            <div className={props.pic ? "promo-pic" : "promo-pic pic2"}></div>
-            <h3>{props.text}</h3>
-        </div>
-    )
-}
+import Promo from "./components/Promo/Promo";
 
 const sizes = ["sm", "lg", "md"];
 const adds = [];
 
 let text = "Полёты собак в космос — серия биологических экспериментов, включавших проведение исследований по возможности полётов на геофизических и космических ракетах живых существ, наблюдение за поведением высокоорганизованных животных в условиях таких полётов, а также, изучение сложных явлений в околоземном пространстве."
 text = text.match(/[^\s,.]+/g);
-console.log(text);
 
 const rand = (n) => Math.floor(Math.random() * n);
 
@@ -35,60 +17,12 @@ while(n--) {
     })
 }
 
-console.log(adds);
-
 const App = () => {
-    // let a = (2 + b) * 2;
-    // let b = (2 + "4").parceInt()
-    return <div title="Doggy">
-        <h1>Hello</h1>
-        <hr />
-        {/* <i>тут есть переменная: {`${a} увеличилась на ${b}`}</i> */}
-        <i/>
-        <h2>
-            <mark>React</mark>
-        </h2>
+    return <div>
         <div className="container">
-            {/* <User name="Vasya" work_age="23" pet="raccoon"/> */}
-            {/* <Promo text="My Promo" type="lg"/>
-            <Promo text="^_^" />
-            <Promo text="Doggy" pic={true}/>
-            <Promo type="sm"/>
-            <Promo text="Leksa is worst teacher in the Moscow" type="sm"/> */}
-            {/* {text="123"} => <Promo text={text}  {...}/> */}
             {adds.map(el => <Promo {...el} type={el.sizes}/>)}
-
         </div>
     </div>
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Rabbit(breed) {
-//     this.age = 2;
-//     this.breed = breed;
-// }
-
-// class Raccoon {
-//     constructor(breed) {
-//         this.age = 2;
-//         this.breed = breed;
-//     }
-// }
-
-// let pet1 = new Rabbit("ololo");
-// let pet2 = new Raccoon("omnomnom");
-
-
