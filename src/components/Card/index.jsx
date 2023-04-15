83d194615e1e563bad3ab2cf5bdd9c10ca601f5f
Leksa <admin@petiteweb.ru>
2023-04-15 15:33:32 +0300

    hotfix

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 // {img, name, price} => props (props.img, props.name, props.price)
 const Card = ({img, name, price}) => {
-    return <a className="card">
+    return <a className="card" href="">
         <img src={img} alt="Картинка" className="card__img"/>
         <span className="card__name">{name}</span>
         <span className="card__price">{price} ₽</span>
